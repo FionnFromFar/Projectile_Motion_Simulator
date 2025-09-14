c1ac20aad7619234ebf14bfc44fe80b84c3221df
Fionn Doheny <fionndoheny73@gmail.com>
2025-09-14 21:04:45 +0100

    I have added a pretty decent amount of new features in this version, including a reload button, amd a start screen so that the user starts the game and is then able to fire the ball. the next steps include the sliders for initial angle and velocity and then a new idea which came to me today which is to properly make it into a game where there will be a hole in the ground and the user has maybe 3 attempts to get it in the hole and each time they do, they score a point and the hole gets smaller - kinda like a golf of sorts game. Very exiting stuff!!!

--- v3_game.py
+++ v3_game.py
@@ -10,16 +10,19 @@ width, height = 1200, 700
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Projectile Simulator")
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 36)
+font_big = pygame.font.SysFont(None, 50)
 
-button_fire = pygame.Rect(1050, 600, 100, 50)
+button_fire = pygame.Rect(1050, 600, 115, 50)
+button_reload = pygame.Rect(1050, 520, 115, 50)
+button_start = pygame.Rect(width//2 - 100, height//2 - 40, 200, 80)
 fired = False
-x, y = 0, 0
-vx, vy = 0, 0
-time = 0
-speed = 20
+speed = 10
 g = 9.81
 angle = 45
 scale = 50
+projectile_stats = []
+game_started = False
 
 #main game loop
 
@@ -32,44 +35,86 @@ while True:
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if button_fire.collidepoint(event.pos) and not fired:
-                fired = True
-                theta = math.radians(angle)
-                vx = speed * math.cos(theta)
-                vy = speed * math.sin(theta)
+            if button_start.collidepoint(event.pos) and not game_started:
+                game_started = True
+                new_proj = {
+                    "x": 0,
+                    "y": 0,
+                    "vx": speed * math.cos(math.radians(angle)),
+                    "vy": speed * math.sin(math.radians(angle)),
+                    "time": 0,
+                    "active": False,
+                    "color": (200, 50, 50)
+                }
+                projectile_stats.append(new_proj)
 
-                #resetting projectile
-                x, y = 0, 0
-                time = 0
-            
+            elif button_reload.collidepoint(event.pos):
+                new_proj = {
+                    "x": 0,
+                    "y": 0,
+                    "vx": speed * math.cos(math.radians(angle)),
+                    "vy": speed * math.sin(math.radians(angle)),
+                    "time": 0,
+                    "active": False,
+                    "color": (200, 50, 50)
+                }
+                projectile_stats.append(new_proj)
+
+            elif button_fire.collidepoint(event.pos):
+                if projectile_stats:
+                    last = projectile_stats[-1]
+                    if (not last["active"]) and (last["y"] == 0):
+                        last["vx"] = speed * math.cos(math.radians(angle))
+                        last["vy"] = speed * math.sin(math.radians(angle))
+                        last["active"] = True
+                        last["time"] = 0
+                        fired = True
+
 
     #Physics section
 
-    if fired:
-        time += dt
-        x = vx * time
-        y = vy * time - ((0.5*g)*(time**2))
+    for proj in projectile_stats:
+        if proj["active"]:
+            proj["time"] += dt
+            proj["x"] = proj["vx"] * proj["time"]
+            proj["y"] = proj["vy"] * proj["time"] - 0.5 * g * (proj["time"]**2)
 
-        if y < 0 and time > 0:
-            y = 0  # ball on da ground
-            fired = False
+            if proj["y"] < 0:
+                proj["y"] = 0
+                proj["active"] = False
+                fired = False
+
+
 
     #Drawing/animation section
-    pygame.draw.rect(screen, (135, 206, 235), (0, 0, width, int(0.65*height)))
-    pygame.draw.rect(screen, (34, 139, 34), (0, int(0.65 * height), width, int(0.35*height)))
-    pygame.draw.rect(screen, (255, 0, 0), button_fire) #red button
-    font = pygame.font.SysFont(None, 36)
-    text = font.render("FIRE", True, (255, 165, 0))
-    screen.blit(text, (button_fire.x + 20, button_fire.y + 10))
+    if not game_started:
+        screen.fill((0, 0, 0))
+        pygame.draw.rect(screen, (135, 206, 235), (0, 0, width, int(0.65*height))) #sky
+        pygame.draw.rect(screen, (34, 139, 34), (0, int(0.65 * height), width, int(0.35*height))) #grass
+        pygame.draw.rect(screen, (0, 255, 0), button_start)
+        text_start = font_big.render("START", True, (0, 128, 0))
+        screen.blit(text_start, (button_start.x + 45, button_start.y + 25))
+        pygame.display.flip()
 
-    if fired or y == 0:
-        start_x = (130)
-        start_y = 75
+    elif game_started:
+        pygame.draw.rect(screen, (135, 206, 235), (0, 0, width, int(0.65*height))) #sky
+        pygame.draw.rect(screen, (34, 139, 34), (0, int(0.65 * height), width, int(0.35*height))) #grass
+        pygame.draw.rect(screen, (255, 0, 0), button_fire) #red button
+        pygame.draw.rect(screen, (0, 100, 0), button_reload) #green reload button same size as 
+        text_fire = font.render("FIRE", True, (255, 165, 0))
+        text_reload = font.render("RELOAD", True, (0, 128, 128))
+        screen.blit(text_fire, (button_fire.x + 30, button_fire.y + 10))
+        screen.blit(text_reload, (button_reload.x + 7, button_reload.y + 10))
         
-        px = start_x + int(x*scale)
-        py = start_y + int(height * 0.6 - (y*scale))
-        pygame.draw.circle(screen, (200, 50, 50), (px, py), 8)
 
-    pygame.display.flip()
+        for proj in projectile_stats:
+            start_x = 130
+            start_y = int(0.65 * height) + 40
+            
+            px = int(start_x + (proj["x"]* scale))
+            py = int(start_y - (proj["y"] * scale))
+            pygame.draw.circle(screen, proj["color"], (px, py), 8)
+
+        pygame.display.flip()
 
     
